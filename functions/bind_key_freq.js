213bcb770ae49c863b5415ed6e10434b759d6fb7
Loïc Béthencourt <loic.bethencourt@ecl15.ec-lyon.fr>
2019-02-15 17:41:35 +0100

    update keyviz + add bind_key

---
+++ functions/bind_key_freq.js
@@ -0,0 +1,47 @@
+function getfreq (freq, key) {
+	var frequency;
+	if (freq[key] == undefined) {
+		frequency = 0;
+	}
+	else {
+		frequency = freq[key];
+	}
+	return frequency;
+}
+
+function binding (keyboard, freq) {
+	keyboard.forEach(function(d,i) {
+		var key = d.v;
+		switch (key) {
+			case '5':
+				keyboard[i]["freq"] = getfreq(freq, '(');
+				break;
+			case '°':
+				keyboard[i]["freq"] = getfreq(freq, key) + getfreq(freq, ')');
+				break;
+			case '_':
+				keyboard[i]["freq"] = getfreq(freq, '-');
+				break;
+			case 'shift_L':
+				keyboard[i]["freq"] = getfreq(freq, 'shift');
+				break;
+			case 'shift_R':
+				keyboard[i]["freq"] = getfreq(freq, 'shift');
+				break;
+			case 'alt_L':
+				keyboard[i]["freq"] = getfreq(freq, 'alt');
+				break;
+			case 'alt_R':
+				keyboard[i]["freq"] = getfreq(freq, 'alt');
+				break;
+			case 'cmd_L':
+				keyboard[i]["freq"] = getfreq(freq, 'cmd');
+				break;
+			case 'cmd_R':
+				keyboard[i]["freq"] = getfreq(freq, 'cmd');
+				break;
+			default:
+				keyboard[i]["freq"] = getfreq(freq, key) + getfreq(freq, key.toLowerCase());
+		}
+	})
+}
