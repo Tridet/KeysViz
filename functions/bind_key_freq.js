9f83bb99946ba83669c4ad227ea4a10d9167095b
Loïc Béthencourt <loic.bethencourt@ecl15.ec-lyon.fr>
2019-02-28 16:56:32 +0100

    update keyboard

--- functions/bind_key_freq.js
+++ functions/bind_key_freq.js
@@ -12,51 +12,53 @@ function getfreq (freq, key) {
 function binding (keyboard, freq) {
 	keyboard.forEach(function(d,i) {
 		var key = d.v;
-		switch (key) {
-			case '5':
-				keyboard[i]["freq"] = getfreq(freq, '(') + getfreq(freq, key) ;
-				break;
-			case '°':
-				keyboard[i]["freq"] = getfreq(freq, key) + getfreq(freq, ')');
-				break;
-			case '_':
-				keyboard[i]["freq"] = getfreq(freq, '-');
-				break;
-			case 'shift_L':
-				keyboard[i]["freq"] = getfreq(freq, 'shift');
-				break;
-			case 'shift_R':
-				keyboard[i]["freq"] = getfreq(freq, 'shift_r');
-				break;
-			case 'alt_L':
-				keyboard[i]["freq"] = getfreq(freq, 'alt');
-				break;
-			case 'alt_R':
-				keyboard[i]["freq"] = getfreq(freq, 'alt_r');
-				break;
-			case 'cmd_L':
-				keyboard[i]["freq"] = getfreq(freq, 'cmd');
-				break;
-			case 'cmd_R':
-				keyboard[i]["freq"] = getfreq(freq, 'cmd_r');
-				break;
-			case '$':
-				keyboard[i]["freq"] = getfreq(freq, '$') + getfreq(freq, '*');
-				break;
-			case '=':
-				keyboard[i]["freq"] = getfreq(freq, '+') + getfreq(freq, key);
-				break;
-			case ':':
-				keyboard[i]["freq"] = getfreq(freq, '/') + getfreq(freq, key);
-				break;
-			case ';':
-				keyboard[i]["freq"] = getfreq(freq, '.') + getfreq(freq, key);
-				break;
-			case ',':
-				keyboard[i]["freq"] = getfreq(freq, '?') + getfreq(freq, key);
-				break;
-			default:
-				keyboard[i]["freq"] = getfreq(freq, key) + getfreq(freq, key.toLowerCase());
+		for (var cat in freq) {
+			switch (key) {
+				case '5':
+					keyboard[i][cat] = getfreq(freq[cat], '(') + getfreq(freq[cat], key) ;
+					break;
+				case '°':
+					keyboard[i][cat] = getfreq(freq[cat], key) + getfreq(freq[cat], ')');
+					break;
+				case '_':
+					keyboard[i][cat] = getfreq(freq[cat], '-');
+					break;
+				case 'shift_L':
+					keyboard[i][cat] = getfreq(freq[cat], 'shift');
+					break;
+				case 'shift_R':
+					keyboard[i][cat] = getfreq(freq[cat], 'shift_r');
+					break;
+				case 'alt_L':
+					keyboard[i][cat] = getfreq(freq[cat], 'alt');
+					break;
+				case 'alt_R':
+					keyboard[i][cat] = getfreq(freq[cat], 'alt_r');
+					break;
+				case 'cmd_L':
+					keyboard[i][cat] = getfreq(freq[cat], 'cmd');
+					break;
+				case 'cmd_R':
+					keyboard[i][cat] = getfreq(freq[cat], 'cmd_r');
+					break;
+				case '$':
+					keyboard[i][cat] = getfreq(freq[cat], '$') + getfreq(freq[cat], '*');
+					break;
+				case '=':
+					keyboard[i][cat] = getfreq(freq[cat], '+') + getfreq(freq[cat], key);
+					break;
+				case ':':
+					keyboard[i][cat] = getfreq(freq[cat], '/') + getfreq(freq[cat], key);
+					break;
+				case ';':
+					keyboard[i][cat] = getfreq(freq[cat], '.') + getfreq(freq[cat], key);
+					break;
+				case ',':
+					keyboard[i][cat] = getfreq(freq[cat], '?') + getfreq(freq[cat], key);
+					break;
+				default:
+					keyboard[i][cat] = getfreq(freq[cat], key) + getfreq(freq[cat], key.toLowerCase());
+			}
 		}
 	})
 }
